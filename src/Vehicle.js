2b4d0667d7cf007f0908117e7bfa75e2e3d14185
Niki Hite <nikihite@comcast.net>
2022-06-09 12:50:43 -0700

    finisjed vehicles.js

--- src/Vehicle.js
+++ src/Vehicle.js
@@ -1,9 +1,16 @@
-export default function Vehicle() {
-  return <div className="vehicle">
-    {/* the Vehcile component has a prop called vehicle. Check to see what string is in this vehicle prop */}
-    {/* if the vehicle props is a car, render a '🚗' emoji */}
-    {/* if the vehicle props is a truck, render a '🚚' emoji */}
-    {/* if the vehicle props is a bus, render a '🚌' emoji */}
-    {/* if the vehicle props is a motorcycle, render a '🏍️' emoji */}
-  </div>;
+export default function Vehicle({ vehicle }) {
+  return (
+    <div className="vehicle">
+      {vehicle === 'car' && '🚗'}
+      {vehicle === 'truck' && '🚚'}
+      {vehicle === 'bus' && '🚌'}
+      {vehicle === 'motorcycle' && '🏍️'}
+    </div>
+  );
 }
+
+{/* the Vehicle component has a prop called vehicle. Check to see what string is in this vehicle prop */}
+{/* if the vehicle props is a car, render a '🚗' emoji */}
+{/* if the vehicle props is a truck, render a '🚚' emoji */}
+{/* if the vehicle props is a bus, render a '🚌' emoji */}
+{/* if the vehicle props is a motorcycle, render a '🏍️' emoji */}
